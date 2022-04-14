445b3e4c2740c653b13bcdfb7e691535ed13ae0b
LaSyntaxe <theo.faedo@gmail.com>
2022-04-14 13:57:01 +0200

    ajout diplayName

--- testUnitaire/src/test/java/TestCalculateur.java
+++ testUnitaire/src/test/java/TestCalculateur.java
@@ -1,24 +1,30 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestCalculateur {
+
+    @DisplayName("Test méthode additionner")
     @Test
-    public void testAdditionner(){
+    void testAdditionner(){
         Assertions.assertEquals(2, Calculateur.additionner(1,1));
     }
 
+    @DisplayName("Test méthode soustraire")
     @Test
-    public void testSoustraire(){
+    void testSoustraire(){
         Assertions.assertEquals(2, Calculateur.soustraire(3,1));
     }
 
+    @DisplayName("Test méthode multiplier")
     @Test
-    public void testMultiplier(){
+    void testMultiplier(){
         Assertions.assertEquals(2, Calculateur.multiplier(1,2));
     }
 
+    @DisplayName("Test méthode diviser")
     @Test
-    public void testDiviser(){
+    void testDiviser(){
         Assertions.assertEquals(2, Calculateur.diviser(4,2));
     }
 }
