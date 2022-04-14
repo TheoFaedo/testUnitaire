a658b2fa4fd9764ea3ac90b6adbad49181b223b8
LaSyntaxe <theo.faedo@gmail.com>
2022-04-14 14:01:45 +0200

    ajout commentaires

--- testUnitaire/src/main/java/Calculateur.java
+++ testUnitaire/src/main/java/Calculateur.java
@@ -1,16 +1,41 @@
 public class Calculateur {
+
+    /**
+     * Méthode qui additionne un entier a à un entier b
+     * @param a : premier entier
+     * @param b : deuxième entier
+     * @return la somme de a et b
+     */
     public static int additionner(int a, int b){
         return a+b;
     }
 
+    /**
+     * Méthode qui soustrait un entier a à un entier b
+     * @param a : entier qu'on souhaite soustraire
+     * @param b : entier à soustraire
+     * @return la différence de a par b
+     */
     public static int soustraire(int a, int b){
         return a-b;
     }
 
+    /**
+     * Méthode qui multiplie un entier a par un entier b
+     * @param a : premier entier
+     * @param b : deuxième entier
+     * @return le produit de a et b
+     */
     public static int multiplier(int a, int b){
         return a*b;
     }
 
+    /**
+     * Méthode qui divise un entier a par un entier b (division euclidienne)
+     * @param a : dividende
+     * @param b : diviseur
+     * @return le quotient de a par b
+     */
     public static int diviser(int a, int b){
         return a/b;
     }
