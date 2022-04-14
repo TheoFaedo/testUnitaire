131b73fb66a2e28a4455cf66c1f8e482c4a08cb3
LaSyntaxe <theo.faedo@gmail.com>
2022-04-14 13:54:15 +0200

    écriture corps methodes

--- testUnitaire/src/main/java/Calculateur.java
+++ testUnitaire/src/main/java/Calculateur.java
@@ -1,17 +1,17 @@
 public class Calculateur {
     public static int additionner(int a, int b){
-        return 1;
+        return a+b;
     }
 
     public static int soustraire(int a, int b){
-        return 1;
+        return a-b;
     }
 
     public static int multiplier(int a, int b){
-        return 1;
+        return a*b;
     }
 
     public static int diviser(int a, int b){
-        return 1;
+        return a/b;
     }
 }
